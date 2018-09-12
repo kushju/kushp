506c1648717ca96216944fade2ea6b815ea66cb3
kuSh madaan <32204425+kushju@users.noreply.github.com>
2018-09-12 18:05:38 +0530

    Add files via upload

---
+++ kushpos.c
@@ -0,0 +1,101 @@
+not able to solve all the functions 
+just written some specific psuedo codes in between
+(not good in c++ (--1));
+**********task 1,4,5,8,10(i have made functions and leter func in main which they fall!!)*****************************************************88*********
+#include<stdc++.h>
+using namespace std;
+class TimeStamp{
+	string dateTime;
+	void put(string dateTimeKush){
+
+      dateTime =dateTimeKush;
+
+}
+string get dateTime{
+	return dateTimeKush;
+
+}
+
+};
+struct data{
+string nodeNumber,nodeId,referenceNodeId,jj="Mr.kush"
+	
+};
+struct node{
+  
+  TimeStamp Datetime;
+  struct Data* data;
+  unsigned int NodeNumber;
+  unsigned int NodeId;
+  struct node *ref;
+  vjc<struct node*> child;
+
+
+struct node *node1 = new node();
+  struct Data *data1=new Data();
+data * node = value(kush);
+string date = datetime();
+    TimeStamp time;
+    time.put(date)
+string encrypt(string value){
+    char key ='*';
+    string output = value;
+    
+    for (int i = 0; i < value.size(); i++)
+        output[i] = value[i] ^ key;
+    
+    return output;
+
+bool printverify(string password,string algo)
+{
+        string password1=encrypt(password);
+        string algo1=encrypt("qwerty");
+        
+        map<int,struct node*>::iterator it=all.begin();
+        int flag=0;
+data1->name=encrypt(name);}
+
+
+
+struct node *genesis(int *right, int *left)
+{
+	parent node has null node;
+
+}
+int kush3=0;
+void longest(struct node* root,int &kushha)
+{
+    	if(root==NULL)
+            return;
+    
+ 		   c++;
+    
+    		for(int i=0; i<root->child.size(); i++)
+            {
+                longest(root->child[i],ma);
+            }
+    
+    	if(kush3>ma)
+            ma=kush3;
+}
+
+
+void merge(struct node* first,struct node * second,vjc<struct node*> hnodes)
+{
+    
+}
+
+
+ {
+            string kush,jj;
+            cout<<"Enter password and algo key";
+            
+            bool k=printverify(password,jj);
+            
+            if(k)
+            cout<<"verified";
+            else
+            cout<<"not verified";
+            
+            cout<<endl;
+        }
